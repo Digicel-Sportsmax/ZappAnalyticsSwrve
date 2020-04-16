4692347cd3349052d1ec10d11821b6d42b2fb6b4
Hussein Yassine <hussein.m.yassin@gmail.com>
2020-04-16 22:43:32 +0300

    initial commit

---
+++ swrveplugin-android/src/main/java/com/cme/swrveplugin_android/PluginConfigurationHelper.kt
@@ -0,0 +1,14 @@
+package com.cme.swrveplugin_android
+
+object PluginConfigurationHelper {
+
+    private var pluginConfiguration: HashMap<String, String> = HashMap()
+
+    fun setConfigurationMap(pluginConfiguration: Map<String, String>) {
+        this.pluginConfiguration.putAll(pluginConfiguration)
+    }
+
+    fun getConfigurationValue(key: String): String? {
+        return this.pluginConfiguration[key]
+    }
+}
---
+++ swrveplugin-android/src/main/java/com/cme/swrveplugin_android/SwrveApplication.kt
@@ -0,0 +1,45 @@
+package com.cme.swrveplugin_android
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.util.Log
+import com.applicaster.app.CustomApplication
+import com.applicaster.digicelloginplugin.SWRVE_ACCOUNT_ID
+import com.applicaster.digicelloginplugin.SWRVE_PRODUCTION_KEY
+import com.applicaster.digicelloginplugin.SWRVE_SANDBOX_KEY
+import com.swrve.sdk.SwrveInitMode
+import com.swrve.sdk.SwrveNotificationConfig
+import com.swrve.sdk.SwrveSDK
+import com.swrve.sdk.config.SwrveConfig
+import com.swrve.sdk.config.SwrveStack
+import java.lang.IllegalArgumentException
+
+class SwrveApplication : CustomApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            val config = SwrveConfig()
+            config.initMode = SwrveInitMode.AUTO
+            config.selectedStack = SwrveStack.EU
+
+            val appId = PluginConfigurationHelper.getConfigurationValue(SWRVE_ACCOUNT_ID)?.toInt() ?: 0
+            val isDebuggable = 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+
+            val apiKey = if (isDebuggable){
+                PluginConfigurationHelper.getConfigurationValue(SWRVE_SANDBOX_KEY)
+            }else{
+                PluginConfigurationHelper.getConfigurationValue(SWRVE_PRODUCTION_KEY)
+            }
+
+            config.setNotificationListener { pushJson ->
+                Log.wtf("Received push", "of body: " + pushJson.toString(1))
+            }
+            SwrveSDK.createInstance(this, appId, apiKey, config)
+        } catch (ex: IllegalArgumentException) {
+            Log.e("SwrveDemo", "Could not initialize the Swrve SDK", ex)
+        }
+    }
+}
