73fc6527420d80492827487f0e1531a2733972a7
Hussein Yassine <hussein.m.yassin@gmail.com>
2020-04-29 15:08:24 +0300

    Refactoring of Swrve Analytics Android

--- zappanalyticsswrve/src/main/java/com/cme/zappanalyticsswrve/SwrveAnalyticsAgent.kt
+++ zappanalyticsswrve/src/main/java/com/cme/zappanalyticsswrve/SwrveAnalyticsAgent.kt
@@ -1,11 +1,22 @@
 package com.cme.zappanalyticsswrve
 
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.os.Build
+import android.util.Log
 import android.widget.Toast
 import com.applicaster.analytics.BaseAnalyticsAgent
+import com.applicaster.app.CustomApplication
 import com.applicaster.util.StringUtil
 import com.google.firebase.FirebaseApp
+import com.swrve.sdk.SwrveInitMode
+import com.swrve.sdk.SwrveNotificationConfig
 import com.swrve.sdk.SwrveSDK
+import com.swrve.sdk.config.SwrveConfig
+import com.swrve.sdk.config.SwrveStack
 import java.util.*
 
 class SwrveAnalyticsAgent : BaseAnalyticsAgent() {
@@ -38,9 +49,55 @@ class SwrveAnalyticsAgent : BaseAnalyticsAgent() {
      */
     override fun initializeAnalyticsAgent(context: Context?) {
         super.initializeAnalyticsAgent(context)
-        context?.let {
-            Toast.makeText(it, "Analytics Agent", Toast.LENGTH_LONG).show()
-            FirebaseApp.initializeApp(context)
+        init(context!!)
+    }
+
+    private fun init(context: Context){
+        val customApp = context as? CustomApplication
+        Toast.makeText(context, "init Application", Toast.LENGTH_LONG).show()
+        FirebaseApp.initializeApp(context)
+        try {
+            val config = SwrveConfig()
+            config.initMode = SwrveInitMode.AUTO
+            config.selectedStack = SwrveStack.EU
+
+            var channel: NotificationChannel? = null
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                channel = NotificationChannel(
+                    "123",
+                    "SportsMax channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                if (customApp?.getSystemService(Context.NOTIFICATION_SERVICE) != null) {
+                    val notificationManager =
+                        customApp?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager.createNotificationChannel(channel)
+                }
+            }
+
+            val notificationConfig = SwrveNotificationConfig.Builder(R.mipmap.ic_launcher, R.mipmap.ic_launcher, channel)
+            config.notificationConfig = notificationConfig.build()
+
+            /*val appId:Int
+            val apiKey: String
+            val isDebuggable = 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+            if (isDebuggable){
+                appId = PluginConfigurationHelper.getConfigurationValue(SWRVE_ACCOUNT_ID_SANDBOX)?.toInt() ?: 0
+                apiKey = PluginConfigurationHelper.getConfigurationValue(SWRVE_SANDBOX_KEY) ?: ""
+            }else{
+                appId = PluginConfigurationHelper.getConfigurationValue(SWRVE_ACCOUNT_ID_PRODUCTION)?.toInt() ?: 0
+                apiKey = PluginConfigurationHelper.getConfigurationValue(SWRVE_PRODUCTION_KEY) ?: ""
+            }*/
+
+            config.setNotificationListener { pushJson ->
+                Log.wtf("Received push", "of body: " + pushJson.toString(1))
+            }
+
+            Toast.makeText(context, "init SwrveSDK", Toast.LENGTH_LONG).show()
+            SwrveSDK.createInstance(customApp, 6883, "eMIb7GMt6Y60SgkeGJSp", config)
+            Toast.makeText(context, "after init SwrveSDK", Toast.LENGTH_LONG).show()
+        } catch (ex: IllegalArgumentException) {
+            Log.e("SwrveDemo", "Could not initialize the Swrve SDK", ex)
         }
     }
 
